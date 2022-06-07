2cd6d87a67b227333ee191c007221ae0a3db6a8f
RullGakGanteng <106739061+RullGakGanteng@users.noreply.github.com>
2022-06-07 14:15:36 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,22 +19,22 @@
   global.autoTyping = false // Ubah Ke true Jika Ingin Saat Ada Yang Chat Bot Auto Mengetik
   global.autoRecord = true // Ubah Ke false Jika Tidak Ingin Saat Ada Yang Chat Bot Auto Merekam
   global.multiplier = 69 // Semakin Besar Angkanya Semakin Sulit Untuk Naik Level
-  global.limitCount = 70 // 70 Dah Pas Banh
+  global.limitCount = 20 // 70 Dah Pas Banh
   global.prefa = ['','.','/','#']
 
   // Other
-  global.ownername = 'Riy' // Ganti Jadi Namamu
-  global.owner = ['6281575886399','6281575886399','6285813334446'] // Kalo Mau Lebih Tambahin Aja
-  global.ownernomer = '6281575886399' // Owner Utama Istilahnya
-  global.botname = 'Hinata Hyuga' // Ganti Jadi Nama Botmu
-  global.packname = 'Hinata Hyuga' // Sticker Weem
-  global.author = 'Riy' // Sticker Weem
+  global.ownername = 'Rull' // Ganti Jadi Namamu
+  global.owner = ['6285859047172','6285859047172','6285859047172'] // Kalo Mau Lebih Tambahin Aja
+  global.ownernomer = '6285859047172' // Owner Utama Istilahnya
+  global.botname = 'SenpaiBot' // Ganti Jadi Nama Botmu
+  global.packname = 'SenpaiBot' // Sticker Weem
+  global.author = 'Rull' // Sticker Weem
   global.webme = 'https://api-riy.herokuapp.com' // Ganti Sama Web Mu
   global.linkgroup = 'https://chat.whatsapp.com/HZhLynp6E9QKNrRFtUp26T' // Ganti Sama Link Gc Mu
   global.sessionName = 'session'
-  global.youtube = 'https://youtube.com/channel/UCtM-pDaaGVRe4BJ7w4qE4Bw' // Ganti Sama Link Yt Mu
-  global.github = 'https://github.com/riychdwayne' // Ganti Sama Link Github Mu
-  global.instagram = 'https://instagram.com/riychdwayne' // Ganti Sama Link Ig mu
+  global.youtube = 'https://youtube.com' // Ganti Sama Link Yt Mu
+  global.github = 'https://github.com' // Ganti Sama Link Github Mu
+  global.instagram = 'https://instagram.com/nagatorobot' // Ganti Sama Link Ig mu
 
   // Background Setiap Menu
   global.bg1 = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=water-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
